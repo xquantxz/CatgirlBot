066f8f129fd2fad754bcd61a5f5554b498ca8c63
xQuantx <xquantxz@gmail.com>
2023-07-12 09:49:47 +0200

    Use embed for /warnings
    Signed-off-by: xQuantx <xquantxz@gmail.com>

--- src/commands/warnings.ts
+++ src/commands/warnings.ts
@@ -1,5 +1,6 @@
-import { CommandInteraction, Interaction, PermissionFlagsBits, SlashCommandBuilder, SlashCommandStringOption, SlashCommandUserOption, User } from "discord.js"
-import { add_warning, db, get_warnings } from "../database";
+import { Colors, CommandInteraction, EmbedBuilder, SlashCommandBuilder, SlashCommandStringOption, SlashCommandUserOption, User } from "discord.js"
+import { db, get_warnings } from "../database";
+import { get_catgirl_image } from "../catgirl";
 
 module.exports = {
 	cmd: new SlashCommandBuilder()
@@ -15,20 +16,31 @@ module.exports = {
         let user = interaction.options.getUser("user") ?? interaction.user;
         let guild = interaction.guild
 
+        let embed = new EmbedBuilder()
+                        .setTitle("Warnings")
+                        .setColor(Colors.Blue)
+                        .setThumbnail(user.avatarURL());
+
         if (!guild) {
             console.error("Something went wrong with warnings command");
             return;
         }
 
         let warnings = get_warnings(db, guild.id, user.id);
+
         let message = warnings.length
                         ? `Warnings for <@${user.id}>\n`
                         : `<@${user.id}> has no warnings`;
+        embed = embed.setDescription(message);
 
         for (let w of warnings) {
-            message += `- ${w.reason}\n`;
+            embed = embed.addFields({
+                name: 'Warning',
+                value:`${w.reason}`,
+                inline: true
+            });
         }
 
-		await interaction.reply(message);
+		await interaction.reply({embeds: [embed]});
 	},
 };
